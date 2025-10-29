708fa7d83e27022bf8d7044a4892fb2b9d5e99d3
Rizki Rohman <rizkirohman54@gmail.com>
2025-10-29 07:05:15 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum consequuntur quam obcaecati eligendi tempora eveniet, delectus pariatur asperiores saepe mollitia dolorum corporis ipsum magni quis molestias, consectetur, dicta explicabo nam?</p>
 </body>
 </html>
