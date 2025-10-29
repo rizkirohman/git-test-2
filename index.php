08fb171ad3a9a6f534af31001f985a1d67f00d83
Rizki Rohman <rizkirohman54@gmail.com>
2025-10-29 07:09:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum consequuntur quam obcaecati eligendi tempora eveniet, delectus pariatur asperiores saepe mollitia dolorum corporis ipsum magni quis molestias, consectetur, dicta explicabo nam?</p>
+    <ul>
+        <li>satu</li>
+    </ul>
 </body>
 </html>
